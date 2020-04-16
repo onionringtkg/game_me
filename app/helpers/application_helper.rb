c6d3daf6ba84b4f4e1678d69b9a4e84c1c87ad50
a733573 <a733573@gmail.com>
2020-04-16 11:31:32 +0900

    カート追加まで

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,42 @@
 module ApplicationHelper
+<<<<<<< HEAD
+=======
+  def page_title
+    title = if @shop.present?
+              @shop.name
+            else
+              "MyApp"
+            end
+    if @title.present?
+      title + " | " + @title
+    else
+      title
+    end
+  end
+
+  def reception_time(shop)
+    if shop.restrict_reception_time
+      format("%<start_hour>d:%<start_min>02d〜%<end_hour>d:%<end_min>02d",
+             start_hour: shop.reception_start_time_hour,
+             start_min: shop.reception_start_time_min,
+             end_hour: shop.reception_end_time_hour,
+             end_min: shop.reception_end_time_min)
+    else
+      "24時間"
+    end
+  end
+
+  def current_carts_items_count
+    current_carts&.map(&:count_items)&.sum || 0
+  end
+
+  def total_amount(cart)
+    cart.cart_items.map { |item| item.price * item.number }.sum +
+      cart.shop.delivery_fee
+  end
+
+  def subtotal(cart_item)
+    cart_item.price * cart_item.number
+  end
+>>>>>>> 92098d1... カートに追加まで
 end
